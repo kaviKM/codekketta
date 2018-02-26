1b31455ed54ef7d693c76dea4a21c6ebc6262c9e
kaviKM <kavibharathi9626@gmail.com>
2018-02-26 12:42:13 +0530

    Create 31.java

---
+++ Beginner/set 1/set 3/set 4/31.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class counting
+{
+public static void main(String[] args)
+{
+String s1= laptop is good;
+ int counter = 0;
+    for (int i = 0; i < str.length(); i++) {
+      if (Character.isLetter(str.charAt(i)))
+        counter++;
+    }.
+    System.out.println(counter + " letters.");
+  }
+}
