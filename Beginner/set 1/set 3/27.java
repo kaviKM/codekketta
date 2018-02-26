61179244dfcbd5621c72cbe0dc5911bbb884cada
kaviKM <kavibharathi9626@gmail.com>
2018-02-26 11:48:52 +0530

    Create 27.java

---
+++ Beginner/set 1/set 3/27.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Numeric
+{
+public static void main(String[] args)
+{
+String string = "kavi";
+boolean numberic = "true";
+try
+{
+Double num = Double.parseDouble(String);
+}
+catch(NumberFormatException e)
+{
+numeric = false;
+}
+if(numeric)
+{
+System.out.println(String + " is a numeric");
+}
+else
+{
+System.out.println(String + " is not a numeric");
+}
+}}
+
